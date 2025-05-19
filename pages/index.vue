633b42a169717f5d2cc558b5914039f502c09bb0
Sotatek-AnVu2 <an.vu2@sotatek.com>
2025-05-19 17:56:56 +0700

    add event to sdk tracking

--- pages/index.vue
+++ pages/index.vue
@@ -3,31 +3,30 @@
 </template>
 
 <script setup lang="ts">
-const sender = ref();
 declare global {
   interface Window {
     webTrackingSDK: any;
   }
 }
-watch(
-  () => sender.value,
-  (newValue) => {
-    console.log(newValue, 'nekkk');
-  },
-  { immediate: true }
-);
 onMounted(async () => {
-  sender.value = await window.webTrackingSDK.init({
+  const sender = await window.webTrackingSDK.init({
     BUID: 'GAMA-aiaas-01',
     property: 'aiaas',
-    sourceProperty: 'TENANT_SOTA',
-    GTMId: '',
-    unleashConfig: {
-      clientKey: '',
-      appName: '',
-      experimentKey: [''],
-    },
+    sourceProperty: '10',
   });
+  sender.passEvent(
+    new window.webTrackingSDK.events.ClickEvent({
+      eventId: '9101',
+      event: 'jb_content_click',
+      pageInfo: { page: 'home' },
+      clickInfo: {
+        sec: 'lovedaily',
+        name: '【Move It】Beat智能運動啞鈴0.5KG | 在家運動好輕鬆',
+        uuid: 'P09220970911',
+        uri: 'https://lovedaily.jollybuy.com/Goods/Detail/P09220970911',
+      },
+    })
+  );
 });
 definePageMeta({
   layout: 'app',
