64b7b64ea850821676a31374e13bfdcbc2953947
Dat Trinh <dat.trinh@sotatek.com>
2025-06-04 16:02:48 +0700

    add format date

--- utils/getDates.ts
+++ utils/getDates.ts
@@ -1,8 +1,8 @@
-export default (dates: string[] = [], separator = '-') => {
+export default (dates: string[] = [], separator = '-', formatStr = '') => {
   if (dates.length > 0) {
     return dates
       .filter((elem) => elem)
-      .map((date) => formatDate(date))
+      .map((date) => formatDate(date, formatStr))
       .join(separator);
   }
   return '';
