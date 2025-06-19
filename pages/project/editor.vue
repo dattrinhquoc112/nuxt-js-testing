64bc522dfa49035279e86b178064e52cc9a4bcc1
NguyenPhuSang <sang.nguyen2@sotatek.com>
2025-06-19 15:55:07 +0700

    fix 359_450 audio popup

--- pages/project/editor.vue
+++ pages/project/editor.vue
@@ -250,14 +250,9 @@ const handleCheckConditionPublish = async () => {
     project.value?.eventEnglishName &&
     project.value.startTime &&
     project.value.endTime;
-  const isFinishedSetupAudio = true;
-  if (isExceedLimit.value) {
-    isOpenReachLimitNoti.value = true;
-  } else if (
-    !!isFinishSetupEvent &&
-    !!isFinishedSetupAudio &&
-    !isExceedLimit.value
-  ) {
+  const isFinishedSetupAudio = editorRef.value.checkIsFinishedSetupAudio();
+
+  if (!!isFinishSetupEvent && !!isFinishedSetupAudio && !isExceedLimit.value) {
     try {
       await handleSaveTemplate(
         t('landing-editor-message-progress_saved'),
@@ -278,8 +273,12 @@ const handleCheckConditionPublish = async () => {
         isOpenReminderPU.value = true;
       }
     }
-  } else {
+  } else if (isExceedLimit.value) {
+    isOpenReachLimitNoti.value = true;
+  } else if (!isFinishSetupEvent) {
     isOpenReminderPU.value = true;
+  } else if (!isFinishedSetupAudio) {
+    editorRef.value?.scrollToSetupAudio();
   }
 };
 
