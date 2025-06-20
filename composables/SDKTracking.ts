081c6e5063691e2a62cda5d702a767e4437a9c4a
Sotatek-AnVu2 <an.vu2@sotatek.com>
2025-06-20 14:00:02 +0700

    update SDK tracking

--- composables/SDKTracking.ts
+++ composables/SDKTracking.ts
@@ -64,7 +64,7 @@ export default function useSDKTracking(sdkInfo: {
         },
         clickInfo: {
           sec: 'ai',
-          type: 'audio_tta',
+          type: 'voiceclone_tts',
           audio_id: audioId,
         },
       })
